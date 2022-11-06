611598a0e45bd3e1581a50e7c795362831b092f6
Carlos <mac@Majortomate.local>
2022-11-05 19:42:32 -0500

    feat: add search page html and css

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link"
+
 function Header () {
   return (
     <div className="header">
       <header className="header__container">
-        <h1>Bookscador</h1>
+        <Link href='/'>
+          <h1>Bookscador</h1>
+        </Link>
         <ul className="nav__container">
           <li>
             <a href="https://www.linkedin.com/in/majortomate/" target='_blank' rel='noreferrer'>Meet the Author</a>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Head from 'next/head'
-import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div>
       <Head>
       <title>Bookscador - Find any book in the world</title>
       </Head>
-      <Header />
       <Hero />
-      <Footer />
     </div>
   )
 }
